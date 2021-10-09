ac552b93b75db17d8159f3a91d8aaffdf4bdda45
Prabhu4tx <38173471+Prabhu4tx@users.noreply.github.com>
2021-10-09 18:34:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/shop')
-def service():
+def service_shop():
     return "Service name: shop"
 
 @app.route('/cart')
-def service():
+def service_cart():
     return "Service name: cart"
 
 @app.route('/payment')
-def service():
+def service_payment():
     return "Service name: payment"
 
 @app.route('/checkout')
-def service():
+def service_checkout():
     return "Service name: checkout"
 
 if __name__ == '__main__':
